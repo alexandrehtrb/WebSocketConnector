f9089ae160f66a4f7b3e2535a0722454c9f67d18
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2024-11-17 22:05:50 -0300

    use TryComplete instead of Complete

--- WebSocketExtensions/WebSocketConnector.cs
+++ WebSocketExtensions/WebSocketConnector.cs
@@ -93,7 +93,7 @@ public abstract class WebSocketConnector
 
     private void ClearAfterClosure()
     {
-        MessagesToSendChannel?.Writer?.Complete();
+        MessagesToSendChannel?.Writer?.TryComplete();
         MessagesToSendChannel = null;
         // To cancel the emission and reception threads:
         this.cancelSendingAndReceivingTokenSource?.Cancel();
