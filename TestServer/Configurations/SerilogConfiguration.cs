0a7e5f9160f5a25fe0b5b34e420d38a15d8fef95
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2026-03-03 11:49:47 -0300

    feature: remove Serilog and Swagger from TestServer

--- TestServer/Configurations/SerilogConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace TestServer.Configurations;
-
-public static class SerilogConfiguration
-{
-    public static IServiceCollection AddSerilogLogger(this IServiceCollection services) =>
-        services.AddSingleton(Serilog.Log.Logger);
-}
--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,6 +1,4 @@
-using TestServer.Configurations;
 using TestServer.Endpoints;
-using Serilog;
 
 namespace TestServer;
 
@@ -8,48 +6,16 @@ public static class Program
 {
     public static int Main(string[] args)
     {
-        var configuration = LoadConfigs();
-        SetupSerilog(configuration);
-
-        try
-        {
-            Log.Information("Starting web host");
-            BuildApp(args).Run();
-            return 0;
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Host terminated unexpectedly");
-            return 1;
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
+        BuildApp(args).Run();
+        return 0;
     }
-
-    private static IConfiguration LoadConfigs() =>
-        new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-
-    private static void SetupSerilog(IConfiguration configuration) =>
-        Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(configuration)
-            .Enrich.FromLogContext()
-            .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name ?? "WebSocketApiExample")
-            .WriteTo.Console()
-            .CreateLogger();
 
     private static WebApplication BuildApp(string[] args)
     {
         var webAppBuilder = WebApplication.CreateBuilder(args);
 
-        webAppBuilder.Host.ConfigureHost();
-        webAppBuilder.Services.ConfigureServices();
+        webAppBuilder.Logging.ClearProviders();
+        webAppBuilder.Logging.AddConsole();
 
         var webApp = webAppBuilder.Build();
         webApp.ConfigureApp();
@@ -57,22 +23,8 @@ public static class Program
         return webApp;
     }
 
-    private static void ConfigureServices(this IServiceCollection services)
-    {
-        services.ConfigureJsonOptions();
-        services.AddSerilogLogger();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-    }
-
-    private static void ConfigureHost(this ConfigureHostBuilder builder) =>
-        builder.UseSerilog();
-
     private static IApplicationBuilder ConfigureApp(this WebApplication app) =>
         app.MapTestEndpoints()
-           .UseSwagger()
-           .UseSwaggerUI()
            .UseDefaultFiles()
            .UseStaticFiles()
            .UseWebSockets(new()
